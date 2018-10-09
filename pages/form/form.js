523b660e2a9a31313e61d3e69c57cf2219f22e71
zhuyc <zhuyc@raiyee.com>
2018-10-10 00:02:27 +0800

    form

--- pages/form/form.js
+++ pages/form/form.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    cities: ['美国', '中国', '巴西', '日本'],
+    index: 0,
+    time: '08:00',
+    date: '1994-11-29',
+    years: ['94', '95', '96', '97', '98', '99', '00', '01', '02', '03', '04', '05'],
+    year: 94
   },
 
   bindgetuserinfo(e) {
@@ -16,6 +21,38 @@ Page({
   },
   resetInfo(e) {
     console.log(e);
+  },
+  checkboxChange(e) {
+    console.log(e);
+  },
+  inputChange(e) {
+    console.log(e.detail)
+    if(e.detail.value=='zyc') {
+      console.log('相等了');
+      return '朱宇晨';
+    }
+  },
+  pickerChange(e) {
+    console.log(e);
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  timePickerChange(e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  datePickerChange(e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  innerPickerChange(e) {
+    console.log(e)
+    this.setData({
+      year: this.data.years[e.detail.value]
+    })
   },
   /**
    * 生命周期函数--监听页面加载
--- pages/swiper/swiper.js
+++ pages/swiper/swiper.js
@@ -6,7 +6,7 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
     ],
     intervalTime: 2000,
-    autoPlay: false
+    autoPlay: true
   },
   swiperd (e) {
     // console.log('滑动了一下');
